ed8dcddc7ca9d3e3f9ce75c7c5bf0f30ecfe7bce
javi <javi@JJ-HPLAPTOP>
2020-04-20 17:33:20 +0200

    First commit. Project setup and Customer entity first method test driven development: testCustomerConstruction.

---
+++ src/main/java/eus/tartanga/tddcrudserversidemaven/entity/Customer.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.tartanga.tddcrudserversidemaven.entity;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.context.Dependent;
+
+/**
+ * Customer TDD entity.
+ * @author javi
+ */
+public class Customer {
+    
+}
---
+++ src/test/java/eus/tartanga/tddcrudserversidemaven/entity/CustomerIT.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eus.tartanga.tddcrudserversidemaven.entity;
+
+import javax.inject.Inject;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.runner.RunWith;
+
+/**
+ * Test class for Customer entity.
+ * @author javi
+ */
+@RunWith(Arquillian.class)
+public class CustomerIT {
+    /**
+     * Deployment archive definition for tests.
+     * @return An Assignable archive base.
+     */
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class,"test.war")
+                .addClass(Customer.class)
+                .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+                //.addAsResource("META-INF/persistence.xml");
+    }
+
+    @Test
+    public void testCustomerConstruction() throws Exception {
+        Customer customer=Customer.class.getConstructor().newInstance();
+        assertNotNull("The bean cannot be constructed!!!",
+                      customer);
+    }
+
+}
